e1c609153d3b50b631c3c10aa415833d62e94726
栗鑫 <lx389029980@outlook.com>
2018-12-24 18:22:54 +0800

    12.24

--- CPractice3/CP3_1.c
+++ CPractice3/CP3_1.c
@@ -1,5 +1,20 @@
-#include <stdio.h>
+﻿#include <stdio.h>
+/*练习基本输入输出函数*/
 int main()
 {
+	char cChar1, cChar2, cChar3, cChar4;
+	cChar1 = 'H';
+	cChar2 = 'e';
+	cChar3 = 'l';
+	cChar4 = 'o';
 
+	putchar(cChar1);
+	putchar(cChar2);
+	putchar(cChar3);
+	putchar(cChar3);
+	putchar(cChar4);
+	putchar('\n');
+
+	system("pause");
+	return 0;
 }
