0d3e30af2cbf1a46d9df907bd02851c427728c40
栗鑫 <lx389029980@outlook.com>
2018-12-24 19:38:49 +0800

    12.24

--- CPractice3/CP3_1.c
+++ CPractice3/CP3_1.c
@@ -1,14 +1,17 @@
 ﻿#include <stdio.h>
+#include <Windows.h>
 /*练习基本输入输出函数*/
 int main()
 {
-	UsePutchar();
+	/*UsePutchar();*/
+	UseGetchar();
 }
 
+/*单字符变量的输出*/
 int UsePutchar()
 {
 	char cChar1, cChar2, cChar3, cChar4;
-	cChar1 = 'H';
+	cChar1 = 'H'; /*注意单字符变量一定要用单引号*/
 	cChar2 = 'e';
 	cChar3 = 'l';
 	cChar4 = 'o';
@@ -18,6 +21,23 @@ int UsePutchar()
 	putchar(cChar3);
 	putchar(cChar3);
 	putchar(cChar4);
+	putchar('\n');
+
+	system("pause");
+	return 0;
+}
+
+/*单字符变量的输入*/
+int UseGetchar()
+{
+	char cChar1;
+	printf("请输入一个字符: ");
+	cChar1 = getchar();
+	putchar(cChar1);
+	putchar('\n');
+	printf("请输入一个字符: ");
+	getchar();
+	putchar(getchar());
 	putchar('\n');
 
 	system("pause");
